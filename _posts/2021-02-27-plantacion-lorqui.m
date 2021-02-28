2f50cacb980b340d48e7f330d3c160d6b19d50f3
gomezsabater <42980959+gomezsabater@users.noreply.github.com>
2021-02-28 22:57:19 +0100

    Create 2021-02-27-plantacion-lorqui.m

---
+++ _posts/2021-02-27-plantacion-lorqui.m
@@ -0,0 +1,23 @@
+---
+layout: post
+title: "Taller de Medio Ambiente.  Plantación de Bosque de Ribera"
+subtitle: "Taller medioambiental"
+background: "/img/posts/bg-lorqui.jpg"
+eventdate: 2020-03-06 09:30:00 +0100
+category: "local"
+tags: "lorqui"
+placeName: "Puente del río, orilla de Lorquí"
+
+
+    
+---
+ ## Colaboran:
+
+El Ayuntamiento de Lorquí, Protección Civil, Confederación Hidrográfica del Segura, Asociación Futuro de la Educación para la Región de Murcia (AFEReM) dentro de las VII Jornadas una Educación para el Siglo XXI (Taller medio ambiental), ANSE, Ecologistas en Acción, Acciona, Asociación Camino al Humanismo y a la Ciencia y otros voluntarios colaboran en este taller medio ambiental beneficioso para los pueblos de Lorquí y Ceutí y, para la mejora del río Segura.  
+
+## PLANTACIÓN DE ÁRBOLES. 
+Sábado día 6 de marzo de 2021, en la margen del río a su paso por el municipio de Lorquí.  
+Lugar de encuentro en el puente, ribera de Lorquí.
+Atendiendo a las medidas sanitarias anticovid19, se plantarán árboles en dos turnos con inscripción previa en el Ayuntamiento de Lorquí: 9,30 – 11,30 h. (20 personas) y  11,00 – 13,30 h. (20 personas). Se necesita nombre, apellidos y DNI. Escribir al correo: promocionsocial@lorqui.es o al teléfono 968.690001 (a la atención de Isabel Torregrosa).
+
+[cartel](img/post/plantacionlorqui.jpg)
